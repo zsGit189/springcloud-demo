f4c7455b33a310cae06d367d0c7a778f5cab25f3
zsGit189 <2295949249@qq.com>
2020-01-02 18:40:04 +0800

    提交到本地

---
+++ guazi-consumer/src/main/java/com/jk/controller/TestController.java
@@ -0,0 +1,43 @@
+package com.jk.controller;
+
+
+
+
+import com.jk.utils.OSSClientUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("order")
+public class TestController {
+
+
+    @RequestMapping("toTest")
+    public String toTest (){
+
+        return "test";
+    }
+
+
+    //图片
+    /**
+     * OSS阿里云上传图片
+     */
+    @RequestMapping("updaloadImg")
+    @ResponseBody
+    public String uploadImg(MultipartFile imgg)throws IOException {
+        if (imgg == null || imgg.getSize() <= 0) {
+            throw new IOException("file不能为空");
+        }
+        OSSClientUtil ossClient=new OSSClientUtil();
+        String name = ossClient.uploadImg2Oss(imgg);
+        String imgUrl = ossClient.getImgUrl(name);
+        String[] split = imgUrl.split("\\?");
+        return split[0];
+    }
+
+}
